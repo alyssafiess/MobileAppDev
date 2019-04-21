3b4430328bf50d47f69e1eae1ab53b4fe211135b
alyssafiess <49376722+alyssafiess@users.noreply.github.com>
2019-04-21 11:44:35 -0500

    Update

--- module 03/project/index.php
+++ module 03/project/index.php
@@ -1,5 +1,4 @@
 <!DOCTYPE html>
-<php>
 <html>
 <head>
 <title>RSVPiDo</title>
@@ -31,25 +30,7 @@
 <div class="main">
   <h3><center>Saturday May 11th, 2019</h3>
   <h4><center>Attendee's</center></h4>
-  <?php
-
-  $slq = "SELECT fname, lname, guestnum FROM `guests`;
-  $result = mysql_query($conn, $sql);
-
-  echo "<div id='label-row'><div class='label'>First Name</div><div class='label'>Last Name</div><dive class='label'>Guest Number</div>"
-
-  if (mysql_query($result) > 0) {
-    while($row = mysql_query($result)) {
-      echo "fname: " . $row["fname"] . "<br>";
-      echo "lname: " . $row["lname"] . "<br>";
-      echo "guestnum: " . $row["guestnum"] . "<br>";
-    }
-  } else {
-    echo "0 results";
-  }
-
-  mysql_close($conn);
-  ?>
+  <p><?php include 'php/attendees.php';?></p>
   <a target="_blank" href="Img4.jpg">
   <thumbimg src="Images/Img4.jpg" alt="Engagement Rings" style="width:150px">
 </a>Sem nisi suscipit primis commodo facilisi ad natoque, ante imperdiet cum ligula duis platea, a fermentum purus feugiat himenaeos cubilia. Sodales sociis consequat lobortis sagittis suscipit facilisis torquent est risus, ante cursus nascetur conubia neque dignissim posuere facilisi phasellus condimentum, iaculis penatibus sollicitudin commodo mollis enim proin nulla. Etiam aenean cum at vivamus erat sociis integer dui nibh class auctor iaculis, ac molestie est condimentum ultricies dignissim parturient venenatis felis lobortis.</p>

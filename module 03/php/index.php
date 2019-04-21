7898e750bc902064a470d8a084ee15bdd60fe690
alyssafiess <49376722+alyssafiess@users.noreply.github.com>
2019-04-21 11:20:24 -0500

    update

--- module 03/php/index.php
+++ module 03/php/index.php
@@ -1,6 +1,6 @@
 <?php
 
-$slq = "SELECT fname, lname, guestnum FROM `guests` WHERE state IN ('IL')";
+$slq = "SELECT fname, lname, guestnum FROM `guests`;
 $result = mysql_query($conn, $sql);
 
 echo "<div id='label-row'><div class='label'>First Name</div><div class='label'>Last Name</div><dive class='label'>Guest Number</div>"

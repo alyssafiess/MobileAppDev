6fd4050379980586a187484be8a579c64048ece7
alyssafiess <49376722+alyssafiess@users.noreply.github.com>
2019-04-21 12:02:52 -0500

    U

--- module 03/project/index.php
+++ module 03/project/index.php
@@ -10,7 +10,7 @@
 <div class=".slidshow-container">
     <img src="Images/Img3.jpg" alt="Wedding Photo">
 </div>
-<div class="topnav" style="padding-left:125px" style="padding-top:10px">
+<div class="topnav" style="padding-left:175px" style="padding-top:10px">
   <a class="active" href="#home">Save the Date</a>
   <a href="#photos">Engagement Photos</a>
   <a href="#directions">Venue Directions</a>

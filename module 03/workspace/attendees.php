efbedbf18c25e122c8984457e70e5ab5f9549d12
alyssafiess <49376722+alyssafiess@users.noreply.github.com>
2019-04-21 20:39:05 -0500

    U

--- module 03/workspace/attendees.php
+++ module 03/workspace/attendees.php
@@ -1,6 +1,18 @@
 <?php
 
-$slq = "SELECT fname, lname, guestnum FROM `guests`;
+$servername = "http://mobileappdev.centralus.cloudapp.azure.com";
+$username = "afadmin";
+$password = "DevNull//1111";
+//open connection
+$conn = new mysqli($servername, $username, $password);
+//check for connection failure
+
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+//retrieve data to display from SQL query
+$slq = "SELECT fname, lname, guestnum FROM `RSVPiDo`.`guests`;
 $result = mysql_query($conn, $sql);
 
 echo "<div id='label-row'><div class='label'>First Name</div><div class='label'>Last Name</div><dive class='label'>Guest Number</div>"
@@ -15,5 +27,5 @@ if (mysql_query($result) > 0) {
   echo "0 results";
 }
 
-mysql_close($conn);
+$mysqli_close($conn);
 ?>
--- module 03/workspace/index.php
+++ module 03/workspace/index.php
@@ -31,7 +31,37 @@
   <h3><center>Saturday May 11th, 2019</h3>
   <h4><center>Attendee's</center></h4>
   <p>
-    <?php include 'attendees.php';?>
+    <?php
+
+    $servername = "http://mobileappdev.centralus.cloudapp.azure.com";
+    $username = "afadmin";
+    $password = "DevNull//1111";
+    //open connection
+    $conn = new mysqli($servername, $username, $password);
+    //check for connection failure
+
+    if ($conn->connect_error) {
+      die("Connection failed: " . $conn->connect_error);
+    }
+
+    //retrieve data to display from SQL query
+    $slq = "SELECT fname, lname, guestnum FROM `RSVPiDo`.`guests`;
+    $result = mysql_query($conn, $sql);
+
+    echo "<div id='label-row'><div class='label'>First Name</div><div class='label'>Last Name</div><dive class='label'>Guest Number</div>"
+
+    if (mysql_query($result) > 0) {
+      while($row = mysql_query($result)) {
+        echo "fname: " . $row["fname"] . "<br>";
+        echo "lname: " . $row["lname"] . "<br>";
+        echo "guestnum: " . $row["guestnum"] . "<br>";
+      }
+    } else {
+      echo "0 results";
+    }
+
+    $mysqli_close($conn);
+    ?>
   </p>
 </center>
 </div>

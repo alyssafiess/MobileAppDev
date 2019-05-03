b28bc16d77d646d963eb0abbb917fd9e4c15e8af
alyssafiess <49376722+alyssafiess@users.noreply.github.com>
2019-05-02 22:36:51 -0500

    Update

---
+++ module 05/project/rsvp.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>RSVPiDo</title>
+<link rel="stylesheet" href="style.css">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+
+<div class=".img">
+      <img src="Images/Img3.jpg" alt="Wedding Photo">
+</div>
+<div id="topnav" style="padding-top:0px">
+  <a href="rsvp.php" class="nav-btn">RSVP</a>
+  <a href="index.html" class="nav-btn">Save the Date</a>
+  <a href="photos.html" class="nav-btn">Engagement Photos</a>
+  <a href="maps.html" class="nav-btn">Venue Directions</a>
+  <a href="stay.html" class="nav-btn">Nearby Lodging</a>
+</div>
+
+<div class="sidenav">
+  <a href="Logo.jpg">
+  <img src="Images/Logo.jpg" alt="RSVPiDo" style="width:150px">
+  </a>
+  <a href="index.html">Save the Date</a>
+  <a href="https://www.target.com/gift-registry/" target=_blank>Gift Registry</a>
+  <a href="contact.html">Contact Us</a>
+</div>
+
+<div class="main">
+  <h2><center>RSVP</center></h2><br>
+  <p>PHP code will go here for guest to RSVP</p>
+  </div>
+</body>
+</html>

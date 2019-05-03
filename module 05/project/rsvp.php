aa8f5ef98a894f9eec3cc1aa1174c5d0d8922dd8
alyssafiess <49376722+alyssafiess@users.noreply.github.com>
2019-05-03 14:50:27 -0500

    Update

--- module 05/project/rsvp.php
+++ module 05/project/rsvp.php
@@ -10,12 +10,13 @@
 <div class=".img">
       <img src="Images/Img3.jpg" alt="Wedding Photo">
 </div>
+
 <div id="topnav" style="padding-top:0px">
-  <a href="rsvp.php" class="nav-btn">RSVP</a>
   <a href="index.html" class="nav-btn">Save the Date</a>
   <a href="photos.html" class="nav-btn">Engagement Photos</a>
   <a href="maps.html" class="nav-btn">Venue Directions</a>
   <a href="stay.html" class="nav-btn">Nearby Lodging</a>
+  <a href="rsvp.php" class="nav-btn">RSVP</a>
 </div>
 
 <div class="sidenav">
@@ -30,7 +31,7 @@
 <div class="main">
   <h2><center>RSVP</center></h2><br>
   <p>PHP code will go here for guest to RSVP</p>
-  </div>
+</div>
 </body>
 <footer>
   <div class="footnav">
